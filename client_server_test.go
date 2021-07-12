cfeead92c0b951333532037ce873c93c72842a15
fys <fangyousong@qq.com>
2021-07-12 16:51:59 +0800

    * benchmark

--- client_server_test.go
+++ client_server_test.go
@@ -16,29 +16,27 @@ func (m *EchoClientHandlerTest) Handle(path string, request Request, responseDat
 
 //跑这个测试前须先在9090端口启动echo_server, echo_server在example/echo_server.go
 func BenchmarkEchoClientServer(t *testing.B) {
-	c := make(chan error, 3)
-	for i := 0; i < 3; i++ {
-		go func() {
-			client, err := NewClient(ClientConfig{
-				MaxConnections:        1000,
-				MaxChannelsPerConn:    10,
-				ChannelPacketQueueLen: 1000,
-				TcpWriteQueueLen:      1000,
-				TcpReadBufferSize:     16 * 1024 * 1024,
-				TcpWriteBufferSize:    16 * 1024 * 1024,
-				TcpConnectTimeout:     time.Second * 3,
-			}, ":9090")
-			if err != nil {
-				c <- fmt.Errorf("connect server fail")
-				return
-			}
-			client.RegisterHandler("/echo_benchmark", &EchoClientHandlerTest{})
-			channel, err := client.NewChannel()
-			if err != nil {
-				c <- fmt.Errorf("new channel fail")
-				return
-			}
-			echoData := []byte(`1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+	for i := 0; i < t.N; i++ {
+		client, err := NewClient(ClientConfig{
+			MaxConnections:        1000,
+			MaxChannelsPerConn:    10,
+			ChannelPacketQueueLen: 1000,
+			TcpWriteQueueLen:      1000,
+			TcpReadBufferSize:     16 * 1024 * 1024,
+			TcpWriteBufferSize:    16 * 1024 * 1024,
+			TcpConnectTimeout:     time.Second * 3,
+		}, ":9090")
+		if err != nil {
+			t.Fatalf("connect server fail")
+			return
+		}
+		client.RegisterHandler("/echo_benchmark", &EchoClientHandlerTest{})
+		channel, err := client.NewChannel()
+		if err != nil {
+			t.Fatalf("new channel fail")
+			return
+		}
+		echoData := []byte(`1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
 			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
 			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
 			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
@@ -51,24 +49,16 @@ func BenchmarkEchoClientServer(t *testing.B) {
 			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
 			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest` + fmt.Sprintf("%d", time.Now().UnixNano()))
 
-			bts, err := channel.DoRequest("/echo_benchmark", NewDefaultRequest(echoData), time.Second)
-			if err != nil {
-				c <- err
-				return
-			}
-			if !bytes.Equal(bts, echoData) {
-				c <- fmt.Errorf("response not same as request")
-				return
-			}
-			channel.Close(nil)
-			client.Close()
-			c <- nil
-		}()
-	}
-	for i := 0; i < 3; i++ {
-		e := <-c
-		if e != nil {
-			t.Fatalf(e.Error())
+		bts, err := channel.DoRequest("/echo_benchmark", NewDefaultRequest(echoData), time.Second)
+		if err != nil {
+			t.Fatalf(err.Error())
+			return
 		}
+		if !bytes.Equal(bts, echoData) {
+			t.Fatalf("response not same as request")
+			return
+		}
+		channel.Close(nil)
+		client.Close()
 	}
 }
