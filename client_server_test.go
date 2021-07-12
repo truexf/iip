bf6e377a71b6b54552cf780e947b67b48b6f9094
fys <fangyousong@qq.com>
2021-07-12 17:04:43 +0800

    * benchmark

--- client_server_test.go
+++ client_server_test.go
@@ -17,48 +17,60 @@ func (m *EchoClientHandlerTest) Handle(path string, request Request, responseDat
 //跑这个测试前须先在9090端口启动echo_server, echo_server在example/echo_server.go
 func BenchmarkEchoClientServer(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		client, err := NewClient(ClientConfig{
-			MaxConnections:        1000,
-			MaxChannelsPerConn:    10,
-			ChannelPacketQueueLen: 1000,
-			TcpWriteQueueLen:      1000,
-			TcpReadBufferSize:     16 * 1024 * 1024,
-			TcpWriteBufferSize:    16 * 1024 * 1024,
-			TcpConnectTimeout:     time.Second * 3,
-		}, ":9090")
-		if err != nil {
-			t.Fatalf("connect server fail")
-			return
-		}
-		client.RegisterHandler("/echo_benchmark", &EchoClientHandlerTest{})
-		channel, err := client.NewChannel()
-		if err != nil {
-			t.Fatalf("new channel fail")
-			return
-		}
-		echoData := []byte(`1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-			1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest` + fmt.Sprintf("%d", time.Now().UnixNano()))
+		//同时3个并发，测试channel是否在并发情况下是否有串扰
+		c := make(chan int, 3)
+		for j := 0; j < 3; j++ {
+			go func() {
+				defer func() {
+					c <- 1
+				}()
+				client, err := NewClient(ClientConfig{
+					MaxConnections:        1000,
+					MaxChannelsPerConn:    10,
+					ChannelPacketQueueLen: 1000,
+					TcpWriteQueueLen:      1000,
+					TcpReadBufferSize:     16 * 1024 * 1024,
+					TcpWriteBufferSize:    16 * 1024 * 1024,
+					TcpConnectTimeout:     time.Second * 3,
+				}, ":9090")
+				if err != nil {
+					t.Fatalf("connect server fail")
+					return
+				}
+				client.RegisterHandler("/echo_benchmark", &EchoClientHandlerTest{})
+				channel, err := client.NewChannel()
+				if err != nil {
+					t.Fatalf("new channel fail")
+					return
+				}
+				echoData := []byte(`1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+					1testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest` + fmt.Sprintf("%d", time.Now().UnixNano()))
 
-		bts, err := channel.DoRequest("/echo_benchmark", NewDefaultRequest(echoData), time.Second)
-		if err != nil {
-			t.Fatalf(err.Error())
-			return
+				bts, err := channel.DoRequest("/echo_benchmark", NewDefaultRequest(echoData), time.Second)
+				if err != nil {
+					t.Fatalf(err.Error())
+					return
+				}
+				if !bytes.Equal(bts, echoData) {
+					t.Fatalf("response not same as request")
+					return
+				}
+				channel.Close(nil)
+				client.Close()
+			}()
 		}
-		if !bytes.Equal(bts, echoData) {
-			t.Fatalf("response not same as request")
-			return
+		for j := 0; j < 3; j++ {
+			<-c
 		}
-		channel.Close(nil)
-		client.Close()
 	}
 }
