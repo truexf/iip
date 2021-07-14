ca228f0599f0e10377fa950cf84859cfabda5fc9
fys <fangyousong@qq.com>
2021-07-14 22:56:14 +0800

    + example: file-download server & client

---
+++ example/file_client/file_client.go
@@ -0,0 +1,170 @@
+// Copyright 2021 fangyousong(方友松). All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"sync/atomic"
+	"time"
+
+	"github.com/truexf/iip"
+)
+
+const (
+	PathDownloadFile = "/download_file"
+)
+
+type FileDownloadTaskClientSide struct {
+	taskId     int64  //unique
+	serverFile string //文件远程路径
+	localFile  string //存储路径
+	// fd              *os.File
+	downloadedBytes int  //已下载字节数
+	completed       bool //下载完成
+	err             error
+}
+
+type FileDownloadRequest struct {
+	iip.DefaultContext
+	TaskId           int64
+	FilePath         string
+	DownloadStartPos int
+}
+
+func NewFileDownloadRequest(task *FileDownloadTaskClientSide) (*FileDownloadRequest, error) {
+	if task.completed {
+		return nil, fmt.Errorf("file downloaded completed, task id %d", task.taskId)
+	}
+	ret := &FileDownloadRequest{TaskId: task.taskId, FilePath: task.serverFile, DownloadStartPos: task.downloadedBytes}
+	ret.SetCtxData("task", task)
+	return ret, nil
+}
+
+// implement Request interface
+func (m *FileDownloadRequest) Data() []byte {
+	bts, _ := json.Marshal(m)
+	return bts
+}
+
+type FileDownloadClient struct {
+	maxTaskId  int64
+	iipChannel *iip.ClientChannel
+}
+
+func NewFileDownloadClient(serverAddr string) (*FileDownloadClient, error) {
+	ret := &FileDownloadClient{}
+	client, err := iip.NewClient(
+		iip.ClientConfig{
+			MaxConnections:        1000,
+			MaxChannelsPerConn:    10,
+			ChannelPacketQueueLen: 1000,
+			TcpWriteQueueLen:      1000,
+			TcpReadBufferSize:     16 * 1024 * 1024,
+			TcpWriteBufferSize:    16 * 1024 * 1024,
+			TcpConnectTimeout:     time.Second * 3,
+		},
+		serverAddr,
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	client.RegisterHandler("/download_file", ret)
+	channel, err := client.NewChannel()
+	if err != nil {
+		return nil, err
+	}
+	ret.iipChannel = channel
+	return ret, nil
+}
+
+func (m *FileDownloadClient) newTaskId() int64 {
+	return atomic.AddInt64(&m.maxTaskId, 1)
+}
+
+// 其格式为： 4字节下载完成标记+4字节文件数据
+func (m *FileDownloadClient) unmarshalFileDownloadResponseChunk(req iip.Request, netResponse []byte) error {
+	task := req.(*FileDownloadRequest).GetCtxData("task").(*FileDownloadTaskClientSide)
+	if len(netResponse) < 4 {
+		task.err = fmt.Errorf("invalid response data, len: %d", len(netResponse))
+		return task.err
+	}
+
+	completed := binary.BigEndian.Uint32(netResponse[:4]) == 1
+	if fd, err := os.OpenFile(task.localFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666); err != nil {
+		panic(fmt.Sprintf("open local file %s fail, %s", task.localFile, err.Error()))
+	} else {
+		fmt.Printf("open file %s ok\n", task.localFile)
+		defer fd.Close()
+		if _, err := fd.Write(netResponse[4:]); err != nil {
+			panic(fmt.Sprintf("write local file %s fail, %s", task.localFile, err.Error()))
+		}
+	}
+
+	task.completed = completed
+	task.downloadedBytes += (len(netResponse) - 4)
+	if completed {
+		fmt.Printf("download save to %s completed\n", task.localFile)
+	} else {
+		//not completed, continue
+		return m.RunTask(task)
+	}
+
+	return nil
+}
+
+func (m *FileDownloadClient) RunTask(task *FileDownloadTaskClientSide) error {
+	request, err := NewFileDownloadRequest(task)
+	if err != nil {
+		return err
+	}
+
+	return m.iipChannel.DoStreamRequest(PathDownloadFile, request)
+}
+
+func (m *FileDownloadClient) Handle(path string, request iip.Request, responseData []byte, dataCompleted bool) error {
+	fmt.Printf("received response chunk %d bytes\n", len(responseData))
+	if !dataCompleted {
+		return nil
+	}
+	err := m.unmarshalFileDownloadResponseChunk(request, responseData)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return err
+}
+
+var (
+	fileServerAddr = flag.String("server", "", "file_client -server [server-addr] -file [server-file]")
+	serverFile     = flag.String("file", "", "file_client -server [server-addr] -file [server-file]")
+)
+
+func main() {
+	flag.Parse()
+	if *fileServerAddr == "" || *serverFile == "" {
+		panic("invaid params")
+	}
+	fileDownloadClient, err := NewFileDownloadClient(*fileServerAddr)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if err := fileDownloadClient.RunTask(&FileDownloadTaskClientSide{
+		taskId:     fileDownloadClient.newTaskId(),
+		serverFile: *serverFile,
+		localFile:  fmt.Sprintf("/tmp/downloaded.file.%d", time.Now().Unix()),
+	}); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	waitChan := make(chan int)
+	<-waitChan
+}
---
+++ example/file_server/file_server.go
@@ -0,0 +1,93 @@
+// Copyright 2021 fangyousong(方友松). All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/truexf/iip"
+)
+
+const (
+	PathDownloadFile = "/download_file"
+	ListenAddr       = ":9091"
+)
+
+type FileDownloadRequest struct {
+	TaskId           int64
+	FilePath         string
+	DownloadStartPos int
+}
+
+type FileServer struct {
+}
+
+func (m *FileServer) Handle(path string, requestData []byte, requestDataCompleted bool) ([]byte, error) {
+	fmt.Printf("request: %s\n", string(requestData))
+	if !requestDataCompleted {
+		return nil, nil
+	}
+	fmt.Println("request data completed")
+	var req FileDownloadRequest
+	if err := json.Unmarshal(requestData, &req); err != nil {
+		fmt.Printf("unmarshal request fail, %s\n", err.Error())
+		return nil, err
+	}
+	fd, err := os.Open(req.FilePath)
+	if err != nil {
+		fmt.Printf("openfile %s fail, %s\n", req.FilePath, err.Error())
+		return nil, err
+	}
+
+	defer fd.Close()
+	fd.Seek(int64(req.DownloadStartPos), io.SeekStart)
+	for {
+		buf := make([]byte, 1024*10)
+		n, err := fd.Read(buf[4:])
+		if err != nil {
+			fmt.Printf("read file fail,%s\n", err.Error())
+			// 读取完成
+			binary.BigEndian.PutUint32(buf, uint32(1))
+			fmt.Println("EOF, send response")
+			return buf[:4], nil
+		} else {
+			// 4字节下载完成标记+文件数据
+			binary.BigEndian.PutUint32(buf, uint32(0))
+			fmt.Println("continue, send response")
+			return buf[:n+4], nil
+		}
+	}
+}
+
+func main() {
+	fmt.Printf("start listen at %s\n", ListenAddr)
+	server, err := iip.NewServer(iip.ServerConfig{
+		MaxConnections:        1000,
+		MaxChannelsPerConn:    10,
+		ChannelPacketQueueLen: 1000,
+		TcpWriteQueueLen:      1000,
+		TcpReadBufferSize:     16 * 1024 * 1024,
+		TcpWriteBufferSize:    16 * 1024 * 1024,
+	}, ListenAddr, nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	svr := &FileServer{}
+	server.RegisterHandler(PathDownloadFile, svr, nil)
+
+	err = server.StartListen()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	waitChan := make(chan int)
+	<-waitChan
+}
